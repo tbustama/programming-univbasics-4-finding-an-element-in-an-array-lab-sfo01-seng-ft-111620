8a643ae264f4b4c43397cb383bcebd72f9db7549
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-02 23:49:00 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,7 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if array.include?(value_to_find)
+    return array.index(value_to_find)
+  else
+    return nil
+  end
 end
